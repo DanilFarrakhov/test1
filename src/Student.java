738202e0e55cc702c5781a94ea47b10e24972b9b
danil <farrahovdanil97@gmail.com>
2024-11-21 14:33:23 +0500

    Firs commit

---
+++ src/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+    int numberbilet;
+    String name;
+    String familia;
+    int yearseducation;
+    double math;
+    double economy;
+    double Angl;
+    double res;
+
+    Student(int numberbilet, String name, String familia, int yearseducation, double math, double economy, double Angl, double res) {
+        this.numberbilet = numberbilet;
+        this.name = name;
+        this.familia = familia;
+        this.yearseducation = yearseducation;
+        this.math = math;
+        this.economy = economy;
+        this.Angl = Angl;
+        this.res = res;
+    }
+
+    Student(String name, String familia, int yearseducation, double math, double economy, double Angl, double res) {
+        this(0, name, familia, yearseducation, math, economy, Angl, res);
+
+    }
+
+    Student(String name, String familia, double math, double economy, double Angl, double res) {
+        this(0, name, familia, 0, math, economy, Angl, res);
+    }
+}
+
+
